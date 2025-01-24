763229b38c6b94e7026a730cbc4a2232ade2164b
Tim Perkins <tim.perkins@nwea.org>
2025-01-24 15:13:27 -0800

    simple-py-server.py:52: [31m-            require_client_auth=True, # as of 2025-01-24 @ 13:19 if this is enabled then it fucks with SSL stuff.[m simple-py-server.py:52: [32m+[m[32m            require_client_auth=True,[m

--- simple-py-server.py
+++ simple-py-server.py
@@ -49,7 +49,7 @@ def serve():
         server_credentials = grpc.ssl_server_credentials(
             [(server_key.encode(), server_cert.encode())],
             root_certificates=client_cert.encode(),
-            require_client_auth=True, # as of 2025-01-24 @ 13:19 if this is enabled then it fucks with SSL stuff.
+            require_client_auth=True,
         )
 
         logger.info("🔒 ✅ Credentials created")
