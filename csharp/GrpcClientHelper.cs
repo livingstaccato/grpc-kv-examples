38a7b9a76baac7c15e14b317e76af971e9932eab
Tim Perkins <tim.perkins@nwea.org>
2025-01-24 20:00:02 -0800

    csharp/GrpcClientHelper.cs | 36 ++++++++++++++++++------------------

--- csharp/GrpcClientHelper.cs
+++ csharp/GrpcClientHelper.cs
@@ -65,24 +65,24 @@ public class GrpcClientHelper : IDisposable
         var channelOptions = new GrpcChannelOptions
         {
             HttpClient = _httpClient,
-            ServiceConfig = new ServiceConfig
-            {
-                MethodConfigs =
-                {
-                    new MethodConfig
-                    {
-                        Names = { MethodName.Default },
-                        RetryPolicy = new RetryPolicy
-                        {
-                            MaxAttempts = 5,
-                            InitialBackoff = TimeSpan.FromSeconds(1),
-                            MaxBackoff = TimeSpan.FromSeconds(5),
-                            BackoffMultiplier = 1.5,
-                            RetryableStatusCodes = { StatusCode.Unavailable }
-                        }
-                    }
-                }
-            },
+            // ServiceConfig = new ServiceConfig
+            // {
+            //     MethodConfigs =
+            //     {
+            //         new MethodConfig
+            //         {
+            //             Names = { MethodName.Default },
+            //             RetryPolicy = new RetryPolicy
+            //             {
+            //                 MaxAttempts = 5,
+            //                 InitialBackoff = TimeSpan.FromSeconds(1),
+            //                 MaxBackoff = TimeSpan.FromSeconds(5),
+            //                 BackoffMultiplier = 1.5,
+            //                 RetryableStatusCodes = { StatusCode.Unavailable }
+            //             }
+            //         }
+            //     }
+            // },
             LoggerFactory = loggerFactory
         };
 
