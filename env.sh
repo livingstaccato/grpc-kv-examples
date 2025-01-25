52dd2f2c03a44c0e72982106f090514f6ed8a923
Tim Perkins <tim.perkins@nwea.org>
2025-01-24 17:43:10 -0800

    env.sh | 8 ++++----

--- env.sh
+++ env.sh
@@ -2,10 +2,10 @@
 
 BASE_PATH=$(pwd)
 
-#ALGO=rsa-
-#ALGO=ec-secp256r1- # this works
-#ALGO=ec-secp384r1- # this works
-ALGO=ec-secp521r1- # this does not with Python 3.13 and Ruby 3.4 on macOS 15.2
+ALGO=rsa-          # this works
+ALGO=ec-secp256r1- # this works
+ALGO=ec-secp384r1- # this works
+# ALGO=ec-secp521r1- # this does not on Python 3.13 and Ruby 3.4 on macOS 15.2
 
 export PLUGIN_CLIENT_CERT="$(cat ./certs/${ALGO}mtls-client.crt)"
 export PLUGIN_CLIENT_KEY="$(cat ./certs/${ALGO}mtls-client.key)"
