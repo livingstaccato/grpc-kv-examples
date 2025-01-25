6fb8e36270ae1df4ec4e54e2059604b3a503175e
Tim Perkins <tim.perkins@nwea.org>
2025-01-25 15:28:54 -0800

    python/proto/sqlite_pb2_grpc.py:7: [31m-from proto.sqlite_pb2 as sqlite__pb2[m python/proto/sqlite_pb2_grpc.py:7: [32m+[m[32mfrom proto.sqlite_pb2 import as sqlite__pb2[m

--- python/proto/sqlite_pb2_grpc.py
+++ python/proto/sqlite_pb2_grpc.py
@@ -4,7 +4,7 @@ import grpc
 import warnings
 
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
-from proto.sqlite_pb2 as sqlite__pb2
+from proto.sqlite_pb2 import as sqlite__pb2
 
 GRPC_GENERATED_VERSION = '1.68.0'
 GRPC_VERSION = grpc.__version__
