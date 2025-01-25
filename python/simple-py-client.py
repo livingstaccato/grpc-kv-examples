3eecb063cceb11d7aa90b75211cf17b9e1d65d6d
Tim Perkins <tim.perkins@nwea.org>
2025-01-25 14:34:19 -0800

    python/simple-py-client.py:57: [31m-            ('grpc.tls_signature_algorithms', 'ECDSA+SHA512:ECDSA+SHA384')[m python/simple-py-client.py:57: [32m+[m[32m            ('grpc.tls_signature_algorithms', 'ECDSA+SHA512:ECDSA+SHA384'),[m

--- python/simple-py-client.py
+++ python/simple-py-client.py
@@ -54,7 +54,7 @@ def main():
             ('grpc.ssl_target_name_override', 'localhost'),
             ('grpc.default_authority', 'localhost'),
             ('grpc.min_tls_version', '1.3'),
-            ('grpc.tls_signature_algorithms', 'ECDSA+SHA512:ECDSA+SHA384')
+            ('grpc.tls_signature_algorithms', 'ECDSA+SHA512:ECDSA+SHA384'),
             ('grpc.use_local_subchannel_pool', 1),
         ]
 
