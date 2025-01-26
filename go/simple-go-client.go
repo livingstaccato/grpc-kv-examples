11b95dd7dafe6fa980e37c1c75d97e2cda554fdf
Tim Perkins <tim.perkins@nwea.org>
2025-01-26 12:13:03 -0800

    go/simple-go-client.go:14: [31m-    "github.com/livingstaccato/grpc-kv-examples"[m go/simple-go-client.go:14: [32m+[m[32m    "./proto"[m

--- go/simple-go-client.go
+++ go/simple-go-client.go
@@ -11,7 +11,7 @@ import (
     "encoding/pem"
     "bytes"
 
-    "github.com/livingstaccato/grpc-kv-examples"
+    "./proto"
     "google.golang.org/grpc"
     "google.golang.org/grpc/credentials"
     "google.golang.org/grpc/keepalive"
