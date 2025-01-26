85c418c305375ae6f0262a92f84af30bfb71d7b1
Tim Perkins <tim.perkins@nwea.org>
2025-01-26 14:06:30 -0800

    go/go-kv-server.go:243: [31m-            tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,[m go/go-kv-server.go:245: [32m+[m[32m            tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,[m

--- go/go-kv-server.go
+++ go/go-kv-server.go
@@ -241,8 +241,8 @@ func createTLSConfig(cert tls.Certificate, certPool *x509.CertPool, logger *log.
             tls.CurveP521,
         },
         CipherSuites: []uint16{
-            tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
             tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+            tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
             tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
         },
     }
