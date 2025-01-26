ef999410c88dfcbc9517370c96350eae7165c4f0
Tim Perkins <tim.perkins@nwea.org>
2025-01-26 12:52:40 -0800

    go/go-kv-server.go:84: [31m-        MinVersion:   tls.VersionTLS12,[m go/go-kv-server.go:84: [32m+[m[32m        MinVersion:   tls.VersionTLS13,[m

--- go/go-kv-server.go
+++ go/go-kv-server.go
@@ -81,7 +81,7 @@ func main() {
         Certificates: []tls.Certificate{cert},
         ClientAuth:   tls.RequireAndVerifyClientCert,
         ClientCAs:    certPool,
-        MinVersion:   tls.VersionTLS12,
+        MinVersion:   tls.VersionTLS13,
         ServerName:   "localhost",
         CipherSuites: []uint16{
             tls.TLS_AES_128_GCM_SHA256,
