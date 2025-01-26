f7d7c160b7152fbf23b4400a36c625257541dd06
Tim Perkins <tim.perkins@nwea.org>
2025-01-26 12:40:46 -0800

    go/go-kv-client.go:135: [31m-        MinVersion:        tls.VersionTLS12,[m go/go-kv-client.go:135: [32m+[m[32m        MinVersion:        tls.VersionTLS13,[m

--- go/go-kv-client.go
+++ go/go-kv-client.go
@@ -132,7 +132,7 @@ func main() {
         Certificates:       []tls.Certificate{clientCert},
         RootCAs:           certPool,
         ServerName:        "localhost",
-        MinVersion:        tls.VersionTLS12,
+        MinVersion:        tls.VersionTLS13,
         CurvePreferences: []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
     }
     log.Printf("✅ TLS configuration complete 🔒")
