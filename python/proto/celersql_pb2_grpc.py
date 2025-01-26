e31381bb3ee36d86fecf1724708e666f1723baeb
Tim Perkins <tim.perkins@nwea.org>
2025-01-25 19:17:24 -0800

    python/proto/celersql_pb2_grpc.py:6: [31m-import celersql_pb2 as celersql__pb2[m python/proto/celersql_pb2_grpc.py:6: [32m+[m[32mimport proto.celersql_pb2 as celersql__pb2[m

--- python/proto/celersql_pb2_grpc.py
+++ python/proto/celersql_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import celersql_pb2 as celersql__pb2
+import proto.celersql_pb2 as celersql__pb2
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 
 GRPC_GENERATED_VERSION = '1.68.0'
