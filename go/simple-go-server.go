0638ac8ec3a78b5d67bf09b96b62041aaafad131
Tim Perkins <tim.perkins@nwea.org>
2025-01-26 11:45:55 -0800

    go/simple-go-server.go | 10 +++++-----

--- go/simple-go-server.go
+++ go/simple-go-server.go
@@ -81,11 +81,11 @@ func main() {
         ClientCAs:    certPool,
         MinVersion:   tls.VersionTLS13,
         ServerName:   "localhost",
-        CipherSuites: []uint16{
-            tls.TLS_AES_128_GCM_SHA256,
-            tls.TLS_AES_256_GCM_SHA384,
-            tls.TLS_CHACHA20_POLY1305_SHA256,
-        },
+        // CipherSuites: []uint16{
+        //     tls.TLS_AES_128_GCM_SHA256,
+        //     tls.TLS_AES_256_GCM_SHA384,
+        //     tls.TLS_CHACHA20_POLY1305_SHA256,
+        // },
     }
 
     // Add keepalive options
