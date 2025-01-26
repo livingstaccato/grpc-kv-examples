02b232b418ca6f50f3d3caf39cdca192e13a5d3f
Tim Perkins <tim.perkins@nwea.org>
2025-01-26 10:21:46 -0800

    New files added: ?? tools/check-certs.sh

---
+++ tools/check-certs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Display the Issuer and Public-Key of all the certificates in the
+# `./certs` directory.
+
+echo "Issuers for all certs:"
+for i in ./certs/*.crt; do
+    openssl x509 -noout -text -in $i | grep -e '(Issuer|Public-Key)'
+    echo
+done
